f2a139e3403f64208585adf08576b61c4ff78465
João Paulo <jpgf@cin.ufpe.br>
2021-08-21 11:13:51 -0300

    refatorando e comentando os passos

--- models/kmeans.py
+++ models/kmeans.py
@@ -10,11 +10,8 @@ class KMeans(object):
         > clf.predict(X_valid)
     """
 
-    def __init__(self, k=2):
-        """
-        Args:
-            k: The number of clusters.
-        """
+    def __init__(self, k=1):
+        """Args: k: The number of clusters."""
         self.k = k
         self.centroids = None
         self.labels = None
@@ -31,22 +28,27 @@ class KMeans(object):
         m, _ = X.shape
 
         for _ in range(max_iter):
+            # Pick k random examples to be the inital cluster centroids.
             centroids = X[np.random.choice(m, self.k, replace=False)]
             labels = np.zeros(m, dtype=int)
 
             while True:
+                # Assign each example to the closest cluster centroid.
                 for i, data in enumerate(X):
                     labels[i] = np.argmin(
                         [self._dist(data, ct) for ct in centroids]
                     )
 
+                # Move each cluster centroid to the mean of the points assigned.
                 prev_centroids = centroids
                 for j in range(self.k):
                     centroids[j] = np.mean(X[np.argwhere(labels == j)], axis=0)
 
+                # Stop if converges.
                 if np.allclose(centroids, prev_centroids):
                     break
 
+            # Pick the clustering that gives the lowest distortion.
             d = self._distortion(X, centroids, labels)
             if d < lowest_distortion:
                 self.centroids, self.labels = centroids, labels
@@ -69,13 +71,21 @@ class KMeans(object):
         return y
 
 
-    def _distortion(self, X, centroids, labels):
-        return sum(
-            [self._dist(data, centroids[labels[i]]) for i, data in enumerate(X)]
-        )
+    def _distortion(self, X, c, y):
+        """Computes the distortion of a clustering.
+
+        Args:
+            X: Training examples of shape (m, n).
+            c: The centroids.
+            y: Class of each training example.
+
+        Returns: Sum of distances between each example and the centroid to
+                 which it has been assigned.
+        """
+        return sum([self._dist(data, c[y[i]]) for i, data in enumerate(X)])
 
 
-    def _dist(self, x, y, m='euclidian'):
+    def _dist(self, x, y, m='e'):
         """Computes the distance between two NumPy arrays.
 
         Args:
@@ -83,12 +93,11 @@ class KMeans(object):
             y: A NumPy array.
             m: Distance measure.
 
-        Returns:
-            Distance between x and y using measure m. Scalar.
+        Returns: Distance between x and y using measure m.
         """
-        if m == 'euclidian':
-            return np.linalg.norm(x - y)
-        elif m == 'manhattan':
-            return np.sum(np.abs(x - y))
+        if m == 'e':
+            return np.linalg.norm(x - y) # Euclidian distance.
+        elif m == 'm':
+            return np.sum(np.abs(x - y)) # Manhattan (cityblock) distance.
         else:
             raise NotImplementedError(f'Measure {m} not implemented.')
